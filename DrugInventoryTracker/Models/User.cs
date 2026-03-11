da2b7486adf063f153f88ad73ee471e68f5b7b7c
Mukul Choudhari <mchoudhari2004@gmail.com>
2026-03-11 19:36:45 +0530

    Add project files.

---
+++ DrugInventoryTracker/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DrugInventoryTracker.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required] public string Username { get; set; } = string.Empty;
+        [Required] public string Email { get; set; } = string.Empty;
+        [Required] public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Pharmacist";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<StockTransaction> StockTransactions { get; set; } = new List<StockTransaction>();
+    }
+}
